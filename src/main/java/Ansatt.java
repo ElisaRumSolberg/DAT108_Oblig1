8c103ecc478074aecb3fd28ea52ea89742859e2c
Elisa Rumeysa Solberg <68196918+ElisaRumSolberg@users.noreply.github.com>
2026-09-02 15:25:10 +0200

    oppg2

--- src/main/java/Ansatt.java
+++ src/main/java/Ansatt.java
@@ -4,4 +4,35 @@ public class Ansatt {
     private Kjonn kjonn;
     private String stilling;
     private int aarslonn;
+
+
+    public Ansatt(String fornavn, String etternavn,
+                  Kjonn kjonn, String stilling, int aarslonn) {
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.kjonn = kjonn;
+        this.stilling = stilling;
+        this.aarslonn = aarslonn;
+    }
+
+    public int getAarslonn() {
+        return aarslonn;
+    }
+
+    public void setAarslonn(int aarslonn) {
+        this.aarslonn = aarslonn;
+    }
+
+    public Kjonn getKjonn() {
+        return kjonn;
+    }
+
+    @Override
+    public String toString() {
+        return fornavn + " " + etternavn
+                + ", " + kjonn
+                + ", " + stilling
+                + ", lønn: " + aarslonn;
+    }
+
 }
--- src/main/java/Oppg2.java
+++ src/main/java/Oppg2.java
@@ -1,2 +1,93 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
 public class Oppg2 {
+    private static void lonnsoppgjor( List<Ansatt> ansatte,  Function<Ansatt, Integer> beregnNyLonn ) {
+
+        for (Ansatt a : ansatte) {
+            int nyLonn = beregnNyLonn.apply(a);
+            a.setAarslonn(nyLonn);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+        //fastTillegg
+        List<Ansatt> ansatte = lagAnsatte();
+
+
+        Function<Ansatt, Integer> fastTillegg =
+                a -> a.getAarslonn() + 10000;
+
+        lonnsoppgjor(ansatte, fastTillegg);
+
+        System.out.println("Etter fast tillegg:");
+        skrivUtAlle(ansatte);
+
+        //et fast prosenttillegg
+
+        List<Ansatt> ansatte2 = lagAnsatte();
+
+        Function<Ansatt, Integer> prosentTillegg =
+                a -> (int) (a.getAarslonn() * 1.05);
+
+        lonnsoppgjor(ansatte2, prosentTillegg);
+
+        System.out.println("\nEtter 5% tillegg:");
+        skrivUtAlle(ansatte2);
+
+
+        // Tillegg for lav lønn
+        List<Ansatt> ansatte3 = lagAnsatte();
+
+        Function<Ansatt, Integer> lavLonnTillegg =
+                a -> a.getAarslonn() < 500000
+                        ? a.getAarslonn() + 20000
+                        : a.getAarslonn();
+
+        lonnsoppgjor(ansatte3, lavLonnTillegg);
+
+        System.out.println("\nEtter tillegg for lav lønn:");
+        skrivUtAlle(ansatte3);
+
+
+        // Fast prosenttillegg for menn
+
+        List<Ansatt> ansatte4 = lagAnsatte();
+
+        Function<Ansatt, Integer> mannProsentTillegg =
+                a -> a.getKjonn() == Kjonn.MANN
+                        ? (int) (a.getAarslonn() * 1.05)
+                        : a.getAarslonn();
+
+        lonnsoppgjor(ansatte4, mannProsentTillegg);
+
+        System.out.println("\nEtter 5% tillegg for menn:");
+        skrivUtAlle(ansatte4);
+
+    }
+
+
+    private static List<Ansatt> lagAnsatte() {
+        return Arrays.asList(
+                new Ansatt("Samsam", "Abdi", Kjonn.KVINNE, "Utvikler", 600000),
+                new Ansatt("Elisa", "Solberg", Kjonn.KVINNE, "Sjef", 800000),
+                new Ansatt("Casper", "Børretzen", Kjonn.MANN, "Tester", 450000),
+                new Ansatt("Martin", "Larsen", Kjonn.MANN, "Utvikler", 550000),
+                new Ansatt("Ida", "Berg", Kjonn.KVINNE, "Prosjektsjef", 900000)
+        );
+    }
+
+
+    private static void skrivUtAlle(List<Ansatt> ansatte) {
+        for (Ansatt a : ansatte) {
+            System.out.println(a);
+        }
+    }
+
+
 }
