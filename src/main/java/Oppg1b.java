c7bb2b6b7a909dfdb682b8c56dccf00a7bded546
Elisa Rumeysa Solberg <68196918+ElisaRumSolberg@users.noreply.github.com>
2026-09-03 11:59:15 +0200

    oppg1b

--- src/main/java/Oppg1b.java
+++ src/main/java/Oppg1b.java
@@ -20,59 +20,49 @@ public class Oppg1b {
     }
 
 
-
     public static void main(String[] args) {
+        System.out.println("Oppgave 1b");
+        System.out.println("\nLøsning med BinaryOperator");
+
         BinaryOperator<Integer> summerFunksjon =
                 (a, b) -> a + b;
 
         int sum = beregn(12, 13, summerFunksjon);
-        System.out.println("Sum: " + sum);
 
         BinaryOperator<Integer> maxFunksjon =
                 (a, b) -> Math.max(a, b);
 
         int max = beregn(-5, 3, maxFunksjon);
-        System.out.println("Max: " + max);
 
         BinaryOperator<Integer> avstandFunksjon =
-                (a,b)->Math.abs(a - b);
+                (a, b) -> Math.abs(a - b);
 
         int avstand = beregn(54, 45, avstandFunksjon);
-        System.out.println("Avstand: " + avstand);
+
+        System.out.println("Sum av 12 og 13: " + sum);
+        System.out.println("Største av -5 og 3: " + max);
+        System.out.println("Avstand mellom 54 og 45: " + avstand);
 
 
+        System.out.println("\nAlternativ løsning med BiFunction");
 
-
-
-
-
-        BiFunction<Integer, Integer, Integer> summerFunksjon1=
+        BiFunction<Integer, Integer, Integer> summerFunksjon1 =
                 (a, b) -> a + b;
-
-        int sum1 = beregn(12, 13, summerFunksjon);
 
         BiFunction<Integer, Integer, Integer> maksFunksjon1 =
                 (a, b) -> Math.max(a, b);
 
-        System.out.println("Sum: " + sum1);
-
-
-
-        int maks1 = beregn(-5, 3, maksFunksjon1);
-
-        System.out.println("Maks: " + maks1);
-        System.out.println("Max: " + maks1);
-
-
-
-
         BiFunction<Integer, Integer, Integer> avstandFunksjon1 =
                 (a, b) -> Math.abs(a - b);
 
-        int avstand1 = beregn(54, 45, avstandFunksjon);
+        int sum1 = beregn(12, 13, summerFunksjon1);
+        int maks1 = beregn(-5, 3, maksFunksjon1);
+        int avstand1 = beregn(54, 45, avstandFunksjon1);
 
-        System.out.println("Avstand: " + avstand1);
+        System.out.println("Sum av 12 og 13: " + sum1);
+        System.out.println("Største av -5 og 3: " + maks1);
+        System.out.println("Avstand mellom 54 og 45: " + avstand1);
     }
-    }
+}
 
 
